4d9c33d5186d34e755d0369118ecdc6e2c392f74
seamia <github.com@seamia.com>
2019-08-06 09:25:05 -0700

    compression library

---
+++ zip/zip.go
@@ -0,0 +1,57 @@
+package zip
+
+import (
+	"bytes"
+	"compress/gzip"
+	"fmt"
+	"io"
+)
+
+const (
+	compressedStreamPrefix = 31
+)
+
+func Compress(what []byte) []byte {
+	var compressed bytes.Buffer
+	writer, err := gzip.NewWriterLevel(&compressed, gzip.BestCompression)
+	if err != nil {
+		return nil
+	}
+	if _, err = writer.Write(what); err != nil {
+		return nil
+	}
+	writer.Close()
+	return compressed.Bytes()
+}
+
+func Decompress(what []byte) ([]byte, error) {
+	if len(what) == 0 {
+		// the source is empty - there is nothing here to decompress
+		return what, nil
+	}
+
+	if what[0] != compressedStreamPrefix {
+		// it doesn't seem to be compressed - return the source
+		if what[0] != byte('{') && what[0] != byte('[') {
+			fmt.Println("hmmmm.... unextected prefix of persisted stream ....")
+		}
+		return what, nil
+	}
+
+	gz, err := gzip.NewReader(bytes.NewBuffer(what))
+	if err != nil {
+		return nil, fmt.Errorf("Read: %v", err)
+	}
+
+	var decompressed bytes.Buffer
+	_, err = io.Copy(&decompressed, gz)
+	errClose := gz.Close()
+	if err != nil {
+		return nil, err
+	}
+	if errClose != nil {
+		return nil, errClose
+	}
+
+	return decompressed.Bytes(), nil
+}
