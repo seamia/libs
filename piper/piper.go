507a34e350c0ae8c494ba722d271e694c48d47c0
seamia <github.com@seamia.com>
2019-08-29 10:59:59 -0700

    adding piper lib

---
+++ piper/piper.go
@@ -0,0 +1,40 @@
+package piper
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+
+    "github.com/seamia/libs/printer"
+)
+
+type (
+    Processor func(string, printer.Printer)
+)
+
+func Run(proc Processor) {
+    info, err := os.Stdin.Stat()
+    if err != nil {
+	panic(err)
+    }
+
+    if info.Mode()&os.ModeCharDevice != 0 || info.Size() <= 0 {
+	fmt.Println("The command is intended to work with pipes.")
+	os.Exit(123)
+    }
+
+    reader := bufio.NewReader(os.Stdin)
+    var output []rune
+
+    for {
+	input, _, err := reader.ReadRune()
+	if err != nil && err == io.EOF {
+	    break
+	}
+	output = append(output, input)
+    }
+
+    all := string(output)
+    proc(all, printer.Stdout)
+}
